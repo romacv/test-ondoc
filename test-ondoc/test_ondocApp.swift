fd51d279b42fb0ecd428ba165bb78427e890de81
Roman Resenchuk <r@resrom.com>
2022-08-23 13:16:54 +0800

    Initial Commit

---
+++ test-ondoc/test_ondocApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ondocApp.swift
+//  test-ondoc
+//
+//  Created by Roman Resenchuk on 23/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct test_ondocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
